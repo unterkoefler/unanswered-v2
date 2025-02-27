7b6cccd10c354aa7f72858729006033df4fb057b
Willy Unterkoefler <w.unterkoefler@gmail.com>
2025-02-26 23:05:18 -0500

    add rss feed

--- app/Api.elm
+++ app/Api.elm
@@ -3,9 +3,15 @@ module Api exposing (routes)
 import ApiRoute exposing (ApiRoute)
 import BackendTask exposing (BackendTask)
 import FatalError exposing (FatalError)
+import Head
 import Html exposing (Html)
 import Pages.Manifest as Manifest
+import Pages
+import Posts
 import Route exposing (Route)
+import Rss
+import Site
+import Time
 
 
 routes :
@@ -13,7 +19,68 @@ routes :
     -> (Maybe { indent : Int, newLines : Bool } -> Html Never -> String)
     -> List (ApiRoute ApiRoute.Response)
 routes getStaticRoutes htmlToString =
-    []
+    [ rss
+        { siteTagline = ""
+        , siteUrl = Site.config.canonicalUrl
+        , title = "Unanswered.blog"
+        , builtAt = Pages.builtAt
+        , indexPage = []
+        }
+        postsBackendTask
+    ]
+
+postsBackendTask : BackendTask FatalError (List Rss.Item)
+postsBackendTask = 
+    Posts.posts
+        |> BackendTask.map
+            (List.map
+                (\(post) ->
+                    { title = post.title
+                    , description = post.description
+                    , url = post.slug
+                    , categories = []
+                    , author = "Willy Unterkoefler"
+                    , pubDate = Rss.Date post.date
+                    , content = Nothing
+                    , contentEncoded = Nothing
+                    , enclosure = Nothing
+                    }
+                )
+            )
+
+
+rss : 
+    { siteTagline : String
+    , siteUrl : String
+    , title : String
+    , builtAt : Time.Posix
+    , indexPage : List String
+    }
+    -> BackendTask FatalError (List Rss.Item)
+    -> ApiRoute.ApiRoute ApiRoute.Response
+rss options itemsRequest =
+    ApiRoute.succeed
+        (itemsRequest
+            |> BackendTask.map
+                (\items ->
+                    Rss.generate
+                        { title = options.title
+                        , description = options.siteTagline
+                        , url = options.siteUrl ++ "/" ++ String.join "/" options.indexPage
+                        , lastBuildTime = options.builtAt
+                        , generator = Just "elm-pages"
+                        , items = items
+                        , siteUrl = options.siteUrl
+                        }
+                )
+        )
+        |> ApiRoute.literal "feed.xml"
+        |> ApiRoute.single
+        |> ApiRoute.withGlobalHeadTags
+            (BackendTask.succeed
+                [ Head.rssLink "/feed.xml"
+                ]
+            )
 
 
 manifest : Manifest.Config
